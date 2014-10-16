f0b92875243cbc6cf95a72ee02b2caf23c21ce38
Rick Lubbers <ik@ricklubbers.nl>
2014-10-16 11:24:49 +0200

    Removed all this default Kamele Framework crap

--- src/modules/main/controllers/home.php
+++ src/modules/main/controllers/home.php
@@ -6,13 +6,7 @@ class Home extends \System\Basecontroller {
     function index($arg = null) {		
         $layout = new \System\Layout('modules/main/views/home');
 
-        $test = $this->loader['\Modules\Main\Models\Example'];
-        $content = $test->exampleMethod();
         
         $layout->render(array('content'=>$content, 'title'=>'Home'));
-    }
-    
-    static function systemStart() {
-        echo 'This is an event that runs because of static listeners. Awesome stuff.';
     }
 }
--- src/modules/main/services/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Modules\Main\Services;
-if (!defined('SYSTEM')) exit('No direct script access allowed');
-
-class Test extends \System\Baseservice {
-    function someFunction() {
-        return 'Hi :D';
-    }
-}

08ddb5c7b23b8835596049c18dd998de80c9ed10
Rick Lubbers <ik@ricklubbers.nl>
2014-10-16 12:07:35 +0200

    Wrote templating service

--- src/modules/main/controllers/home.php
+++ src/modules/main/controllers/home.php
@@ -3,10 +3,14 @@ namespace Modules\Main\Controllers;
 if (!defined('SYSTEM')) exit('No direct script access allowed');
 
 class Home extends \System\Basecontroller {
+	
+	private $template;
+	
+	function __construct() {
+		$this->template = $this->loader['\Modules\Main\Services\Templating']->getTemplateDirectory();
+	}
+	
     function index($arg = null) {		
-        $layout = new \System\Layout('modules/main/views/home');
-
-        
-        $layout->render(array('content'=>$content, 'title'=>'Home'));
+        echo $this->template;
     }
 }
---
+++ src/modules/main/services/templating.php
@@ -0,0 +1,48 @@
+<?php
+namespace Modules\Main\Services;
+use \System\Baseservice;
+use \Exception;
+
+if (!defined('SYSTEM')) exit('No direct script access allowed');
+
+/**
+ * Templating service
+ * Handles anything that has to do with templating
+ * 
+ * @package     Kamele CMS
+ * @subpackage  Main
+ * @author      Rick Lubbers <me@ricklubbers.nl>
+ * @since       0.1
+ */
+class Templating extends Baseservice {
+    
+    private $template_base_dir = 'modules/templates/';
+    private $template;
+    
+    public function __construct() {
+        $template = $this->loader['\Modules\Main\Models\Settings']->getSetting('template');
+        if ($template == null or $this->templateExists($template) == false) {
+            if ($this->templateExists('default')) {
+                $this->template = 'default';
+            }
+            else {
+                throw new Exception('Cannot find template');
+            }
+        }
+        else {
+            $this->template = $template;
+        }
+    }
+    
+    public function getTemplateDirectory() {
+        return $this->template;
+    }
+    
+    private function templateExists($dir) {
+        if (file_exists($this->template_base_dir.$dir) and is_dir($this->template_base_dir.$dir)) {
+            return true;
+        }
+        return false;
+    }
+    
+}

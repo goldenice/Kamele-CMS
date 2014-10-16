a50adde72ea28797794d3f7041724076df9565cd
Rick Lubbers <ik@ricklubbers.nl>
2014-10-16 11:55:06 +0200

    Wrote settings model for handling settings with the database

---
+++ src/modules/main/models/settings.php
@@ -0,0 +1,38 @@
+<?php
+namespace Modules\Main\Models;
+use \System\Basemodel;
+
+if (!defined('SYSTEM')) exit('No direct script access allowed');
+
+/**
+ * Settings model
+ * Gets and puts settings into the database
+ * 
+ * @package 	Kamele CMS
+ * @subpackage	Main
+ * @author		Rick Lubbers <me@ricklubbers.nl>
+ * @since		0.1
+ */
+class Settings extends Basemodel {
+    
+    public function getSetting($key) {
+        $result = $this->db->fetchAssoc($this->db->safeQuery("SELECT * FROM `settings` WHERE `key`=>:key LIMIT 0,1", array('key'=>$key)));
+        if ($result === null) {
+            return null;
+        }
+        else {
+            return $result['value'];
+        }
+    }
+    
+    public function updateKey($key, $value) {
+    	$result = $this->db->safeQuery("INSERT INTO `settings` (key, value) VALUES (:key, :value) ON DUPLICATE KEY UPDATE `value`=:value");
+    	if ($result == false || $result == null) {
+    		return false;
+    	}
+    	else {
+    		return true;
+    	}
+    }
+    
+}

903035336dd7c6948828b54cf87fcf598648e868
Rick Lubbers <ik@ricklubbers.nl>
2014-10-21 16:59:49 +0200

    Implemented page viewing

--- src/modules/main/controllers/page.php
+++ src/modules/main/controllers/page.php
@@ -9,7 +9,18 @@ if (!defined('SYSTEM')) exit('No direct script access allowed');
 class Page extends Controller {
     
     function view($arg = null) {
-        // TODO: implement this
+        if (!is_numeric($arg[0]))
+            $page = $this->loader['\Modules\Main\Models\Pages']->getVisibleByAlias($arg[0]);
+        else
+            $page = $this->loader['\Modules\Main\Models\Pages']->getVisibleById($arg[0]);
+        
+        if ($page == null) {
+            // TODO: implement 404
+        }
+        else
+            $this->output['content'] = $this->layout->renderPart('modules/main/views/pages/single', $page);
+        
+        $this->render();
     }
     
 }
